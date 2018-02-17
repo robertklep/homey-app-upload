b88a18bc083a6322c2d5368973023df0c9ff0363
Robert Klep <robert@klep.name>
2018-02-17 17:29:46 +0100

    Removed `watch` for the time being; try to auto-discover Homey URL

--- packages/homey-app-upload-cli/lib/cli.js
+++ packages/homey-app-upload-cli/lib/cli.js
@@ -1,12 +1,23 @@
 const fs         = require('fs');
 const path       = require('path');
 const { docopt } = require('docopt');
+const ospath     = require('ospath');
 const Uploader   = require('./uploader');
 
+// Read docopt file.
+let docoptFile = fs.readFileSync(path.join(__dirname, 'docopt.txt'), 'utf8');
+
+// Try to retrieve Homey settings.
+try {
+  let settings = require(path.join(ospath.data(), 'com.athom.athom-cli', 'settings.json'));
+  let homeyUrl = settings.homey.localUrl.replace(/:\d+$/, '');
+  docoptFile = docoptFile.replace('{{ homeyUrl }}', `[default: ${ homeyUrl }]`);
+} catch(e) {
+  docoptFile = docoptFile.replace('{{ homeyUrl }}', '');
+}
+
 // Parse command line options.
-let opts = docopt(fs.readFileSync(path.join(__dirname, 'docopt.txt'), 'utf8'), {
-  version : require('../package').version
-});
+let opts = docopt(docoptFile, { version : require('../package').version });
 
 module.exports = async () => {
   return new Uploader(opts);
